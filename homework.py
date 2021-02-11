4f26711ca816d5094647175ed51268fda6a8c284
Avilov Oleg <dangerousmonk@yandex.ru>
2021-02-11 22:39:00 +0300

    PEP8

--- homework.py
+++ homework.py
@@ -72,7 +72,10 @@ def main():
                     new_homework.get('homeworks')[0]
                 ), bot)
                 logging.info('Отправлено сообщение')
-            current_timestamp = new_homework.get('current_date', current_timestamp)
+            current_timestamp = new_homework.get(
+                'current_date',
+                current_timestamp
+            )
             time.sleep(1200)
 
         except requests.exceptions.RequestException as error:
