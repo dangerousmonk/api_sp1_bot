da420314376d86179bec946e31d068428cc73086
Avilov Oleg <dangerousmonk@yandex.ru>
2021-02-13 16:17:29 +0300

    Add logging config

--- helpers.py
+++ helpers.py
@@ -11,3 +11,38 @@ class TokenFormatter(logging.Formatter):
     def format(self, record):
         original = logging.Formatter.format(self, record)
         return self._filter(original)
+
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'loggers': {
+        '': {
+                'level': 'DEBUG',
+                'handlers': [
+                    'debug_rotating_file_handler',
+                ]
+             },
+        '__main__': {
+            'level': 'DEBUG',
+            'handlers': ['debug_rotating_file_handler'],
+        }
+    },
+    'formatters': {
+        'token': {
+            '()': TokenFormatter,
+            'format': '%(asctime)s, %(levelname)s, %(message)s, %(name)s',
+        },
+    },
+    'handlers': {
+        'debug_rotating_file_handler': {
+            'level': 'DEBUG',
+            'formatter': 'token',
+            'class': 'logging.handlers.RotatingFileHandler',
+            'filename': 'program.log',
+            'mode': 'a',
+            'maxBytes': 50000000,
+            'backupCount': 5,
+        },
+    },
+}
